d89674ece52def02788a783ed683e630741d738c
Moyowaaaa <moyowa7@icloud.com>
2023-01-19 22:58:42 +0100

    loading optimization

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="home">
+    <div class="home" v-if="isLoaded">
      
         <PreloaderVue />
         <TitleSectionVue />
@@ -21,9 +21,40 @@ import ShowcaseSection from '../components/ShowcaseSection.vue'
 import SubscribeSection from '../components/SubscribeSection.vue'
 import AboutSection from '../components/AboutSection.vue'
 import FeaturedSection from '../components/FeaturedSection.vue'
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import cinzelFont from '../assets/fonts/Cinzel-Bold.otf'
+import cormorantFont from '../assets/fonts/Cormorant-Medium.otf'
+import titlePic1 from '../assets/images/titlepic1.png'
+import titlePic2 from '../assets/images/titlepic2.png'
+import titlePic3 from '../assets/images/titlepic3.png'
+
+const isLoaded = ref<boolean>(false)
+const assets = ref([
+    titlePic1,titlePic2,titlePic3
+])
 
 
+
+onMounted(() => {
+    const promises:any[] = []
+    assets.value.forEach(asset => {
+        const img = new Image()
+        img.src = asset
+        promises.push(new Promise((resolve, reject) => {
+          img.onload = resolve
+          img.onerror = reject
+        }))        
+    })
+    new Promise((resolve,reject) => {
+        const font = new FontFace('cinzel',`url(${cinzelFont})`)
+        font.load().then(resolve,reject)
+        const font2 =  new FontFace('cormorant',`url(${cormorantFont})`)
+        font2.load().then(resolve,reject)
+    })
+    Promise.all(promises).then(() => {
+        isLoaded.value = true
+    })
+})
 
 
 </script>
@@ -33,5 +64,6 @@ import { ref } from 'vue';
     display: flex;
     flex-direction: column;
     overflow-x: hidden;
+    
 }
 </style>
